fdd198e5a258699b6adab14d8d2185af2ffae941
Zoz780 <zoltan0108@gmail.com>
2017-11-21 18:46:27 +0100

    Implementation of animation

---
+++ animation/Vec3.cpp
@@ -0,0 +1,13 @@
+#include "Vec3.h"
+
+Vec3::Vec3()
+{
+}
+
+void Vec3::InitValues(float _x, float _y, float _z)
+{
+	x = _x;
+	y = _y;
+	z = _z;
+}
+
---
+++ animation/Vec3.h
@@ -0,0 +1,12 @@
+#pragma once
+class Vec3
+{
+public:
+	Vec3();
+	float x = 0;
+	float y = 0;
+	float z = 0;
+
+	void InitValues(float _x, float _y, float _z);
+};
+
